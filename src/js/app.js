8e1ac524bfa7b7310d15a09437de08e4fdf9c9a4
Alllena <khatkevich.alena@gmail.com>
2023-05-16 18:23:14 +0300

    Start of project

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import * as flsFunctions from "./modules/function.js";
+
+flsFunctions.checkWebPSupport();
+
+import Swiper, { Navigation, Pagination } from "swiper"; // Подключение слайдера (библиотека)
