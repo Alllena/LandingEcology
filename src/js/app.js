7962557e12d16aa4ceba7b88ee77ba86c40d14ec
Alllena <khatkevich.alena@gmail.com>
2023-05-22 15:37:45 +0300

    add footer

--- src/js/app.js
+++ src/js/app.js
@@ -7,3 +7,48 @@ createPagination();
 updateSlider();
 
 import Swiper, { Navigation, Pagination } from "swiper"; // Подключение слайдера (библиотека)
+
+//  отправка сообщения в телеграмм
+
+const telegramLink = document.getElementById("telegram-link");
+
+telegramLink.addEventListener("click", (event) => {
+  event.preventDefault(); // предотвратить выполнение действия по умолчанию
+  const phoneNumber = "+375445606121"; // замените на номер телефона, на который хотите отправить сообщение
+  const messageText = encodeURIComponent(
+    "Привет! Это сообщение из моей ссылки."
+  ); // текст сообщения
+  const telegramUrl = `https://t.me/share/url?url=tel:%2B${phoneNumber}&text=${messageText}`;
+
+  // Открываем Telegram с помощью ссылки
+  window.open(telegramUrl, "_blank");
+});
+
+//плавный  скролл страницы
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
+// отображение heder
+const header = document.querySelector(".header");
+let lastScrollPosition = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScrollPosition = window.pageYOffset;
+
+  if (currentScrollPosition > lastScrollPosition) {
+    // скролл вниз
+    header.classList.add("header-hidden");
+  } else {
+    // скролл вверх
+    header.classList.remove("header-hidden");
+  }
+
+  lastScrollPosition = currentScrollPosition;
+});
