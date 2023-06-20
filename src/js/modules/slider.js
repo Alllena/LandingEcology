79154eee5ddc7cfb058d7b9b0e4215054004ff3b
Alllena <khatkevich.alena@gmail.com>
2023-06-20 09:45:11 +0300

    replaced site colors

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -10,7 +10,7 @@ export function updateSlider() {
     currentIndex * 100
   }%)`;
   pagination.childNodes.forEach((dot, index) => {
-    dot.style.backgroundColor = index === currentIndex ? "#000099" : "#0000ff";
+    dot.style.backgroundColor = index === currentIndex ? "#f57771" : "#f57771";
   });
 }
 
